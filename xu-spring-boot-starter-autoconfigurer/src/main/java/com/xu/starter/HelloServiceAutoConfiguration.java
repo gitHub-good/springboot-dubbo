bee610c41b8cb165039c0b02dc66f25337c02bdc
徐亮亮 <1156033582@qq.com>
2018-10-17 22:24:20 +0800

    spring boot 整合mybatis项目Demo

---
+++ xu-spring-boot-starter-autoconfigurer/src/main/java/com/xu/starter/HelloServiceAutoConfiguration.java
@@ -0,0 +1,22 @@
+package com.xu.starter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnWebApplication //web应用才生效
+@EnableConfigurationProperties(HelloProperties.class)/*可以使HelloProperties配置类在该类中使用*/
+public class HelloServiceAutoConfiguration {
+
+    @Autowired
+    HelloProperties helloProperties;
+    @Bean
+    public HelloService helloService(){
+        HelloService service = new HelloService();
+        service.setHelloProperties(helloProperties);
+        return service;
+    }
+}
